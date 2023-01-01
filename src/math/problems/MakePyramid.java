f7f13c9172ce44ff5eec32e3bd6bb3f22d13b5ee
baruamousumi123 <baruamousumi@aol.com>
2022-12-31 19:54:40 -0500

    midterm

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,7 +1,7 @@
 package math.problems;
 import java.util.Scanner;
-public class MakePyramid {
 
+public class MakePyramid {
     public static void main(String[] args) {
 
         /*   Implement a large Pyramid of stars in the screen with java.
@@ -14,15 +14,21 @@ public class MakePyramid {
          * * * * * *
 
          */
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= i; ++j) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = n - i; j > 1; j--) {
+
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
 
         }
+
     }
 }
